468879154295748062e82ae04f9ea6b5b5186b77
harshitm98 <harshitmah98@gmail.com>
2018-08-22 19:53:19 +0530

    Can surpass reCaptcha (manually) after 3 tries

--- dribbble_sign_up.py
+++ dribbble_sign_up.py
@@ -2,9 +2,10 @@ import random
 
 from selenium import webdriver
 import proxy_lister
+from selenium.webdriver.common.proxy import *
 
-# proxy = proxy_lister.Proxy()
-# proxy.scrapForLiveProxy()
+proxy = proxy_lister.Proxy()
+proxy.scrapForLiveProxy()
 with open('proxy_list.txt') as file:
     ip_list = file.read().split('\n')
     file.close()
@@ -16,13 +17,23 @@ def sign_up(fullname, username, password, emailid):
     ip_port = ip_port.split(' ')
     ip = ip_port[0]
     port = ip_port[1]
-    PROXY = "{}:{}".format(ip, port)
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("--test-type")
-    driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
-    driver.get('https:dribbble.com/signup/new')
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--ignore-certificate-errors')
+    # options.add_argument("--test-type")
+    # options.add_argument('--proxy-server=%s' % PROXY)
+    # driver = webdriver.Chrome(chrome_options=options, executable_path='/usr/bin/chromedriver')
+    # driver.get('https:dribbble.com/signup/new')
+
+    driver = webdriver.Firefox()
+    profile = webdriver.FirefoxProfile('/home/fake_batman/.mozilla/firefox/8n4a3ph8.default')
+    profile.set_preference("network.proxy.type", 1)
+    profile.set_preference("network.proxy.http", ip)
+    profile.set_preference("network.proxy.http_port", port)
+    profile.set_preference("network.proxy.ssl", ip)
+    profile.set_preference("network.proxy.ssl_port", port)
+    driver = webdriver.Firefox(firefox_profile=profile)
+    driver.get('https://dribbble.com/signup/new')
 
     if len(username) >= 20:
         username = username[:20]
@@ -35,31 +46,31 @@ def sign_up(fullname, username, password, emailid):
     usrnameField.send_keys(username)
     passwordField.send_keys(password)
     emailField.send_keys(emailid)
-    if input('Enter \'v\': '):
-        print("Closing driver...")
-        driver.close()
+    input("Press enter: ")
+    driver.find_element_by_xpath('//*[@id="user_location"]').send_keys(random.choice(["Mumbai", "Delhi", "Kolkata", "Bangalore", "Chennai", "Vellore", "Munsa", "vaskc"]))
+    input('Enter something: ')
+    print("Closing driver...")
 
 
 with open('credentials.txt', 'r') as file:
     credentials = file.read().split('\n')
-    file.close()
 
 with open('filled_up_dribbble.txt', 'r+') as file:
     filled_cred = file.read().split('\n')
-    file.close()
+
 count = 0
 while count < 150:
     credential = credentials[count].split(' ')
-    if credential[2] not in filled_cred:
+    username = credential[2]
+    username = username.replace('.', '_')
+    if credentials not in filled_cred:
         fullname = credential[0] + ' ' + credential[1]
         emailid = credential[2] + '@yandax.com'
-        username = credential[2]
         password = credential[3]
         sign_up(fullname, username, password, emailid)
         with open('filled_up_dribbble.txt', 'a+') as file:
             file.write('\n')
-            file.write(credential[2])
-            file.close()
+            file.write(username)
         count += 1
     else:
         count += 1
--- proxy_lister.py
+++ proxy_lister.py
@@ -14,23 +14,24 @@ class Proxy:
         driver.get('https://free-proxy-list.net/')
         search = driver.find_element_by_xpath('//*[@id="proxylisttable_filter"]/label/input')
         search.send_keys('India')
-        # with open('proxy_list.txt', 'w+') as file:
         i = 1
         lists = []
         while True:
             try:
-                path_ip = '//*[@id="proxylisttable"]/tbody/tr[' + str(i) + ']/td[1]'
-                ip = driver.find_element_by_xpath(path_ip).text
-                path_port = '//*[@id="proxylisttable"]/tbody/tr[' + str(i) + ']/td[2]'
-                port = driver.find_element_by_xpath(path_port).text
-                lists.append([ip, port])
+                path_stem = '//*[@id="proxylisttable"]/tbody/tr['
+                proxy_anonymity = driver.find_element_by_xpath(path_stem + str(i) + ']/td[5]').text
+                if proxy_anonymity == 'elite proxy':
+                    path_ip = path_stem + str(i) + ']/td[1]'
+                    ip = driver.find_element_by_xpath(path_ip).text
+                    path_port = path_stem + str(i) + ']/td[2]'
+                    port = driver.find_element_by_xpath(path_port).text
+                    lists.append([ip, port])
                 i += 1
             except Exception as e:
                 print("Ran into error", e)
                 break
-        with open('proxy_list.txt', 'w+') as file:
-            for content in lists:
-                ip_port = '\n' + content[0] + ' ' + content[1]
-                file.write(ip_port)
-            file.close()
+        for content in lists:
+            ip_port = content[0] + ' ' + content[1] + '\n'
+            with open('proxy_list.txt', 'w+') as file:
+                    file.write(ip_port)
         driver.close()
